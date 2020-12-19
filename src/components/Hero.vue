7b4eea8f29e675b3ccc33f54df6133fe5e69edc6
rmcfarlin <rmcfarlin@robertmcfarlin.com>
2020-12-19 07:43:59 -0500

    Finished Hero Section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,37 +1,63 @@
 <template>
-    <div id="item" class="item">
-        <div id="hero" class="item carousel carousel-dark slide" data-bs-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-bs-target="#hero" data-bs-slide-to="0" class="active"></li>
-                <li data-bs-target="#hero" data-bs-slide-to="1"></li>
-                <li data-bs-target="#hero" data-bs-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner" style="height: 85vh">
-                <div class="carousel-item active" style="height: 85vh" data-bs-interval="3000">
-                    <g-image src="https://i.imgur.com/2xIvCzj.jpg" class="d-block w-100" alt="Accounting Image" />
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5 class="text-white fs-1 text-center">Accountant</h5>
-                    </div>
+    <div id="hero" class="carousel slide" data-bs-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-bs-target="#hero" data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#hero" data-bs-slide-to="1"></li>
+            <li data-bs-target="#hero" data-bs-slide-to="2"></li>
+            <li data-bs-target="#hero" data-bs-slide-to="3"></li>
+        </ol>
+        <div class="carousel-inner" style="height: 85vh">
+            <div class="carousel-item active" style="height: 85vh" data-bs-interval="3000">
+                <g-image src="https://i.imgur.com/2xIvCzj.jpg" class="d-block w-100 item-1" style="height: 85vh" alt="Business Image" />
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-white fs-1 text-center pb-5">Accountant</h5>
                 </div>
             </div>
-            <a class="carousel-control-prev" href="#hero" role="button" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#hero" role="button" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </a>
+
+            <div class="carousel-item" style="height: 85vh" data-bs-interval="3000">
+                <g-image src="https://i.imgur.com/aUdNIKM.jpg" class="d-block w-100 item" style="height: 85vh" alt="Airplane Image" />
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-white fs-1 text-center pb-5">Airplane Enthusiast</h5>
+                </div>
+            </div>
+
+            <div class="carousel-item" style="height: 85vh" data-bs-interval="3000">
+                <g-image src="https://i.imgur.com/ET6N4ec.jpg" class="d-block w-100 item" style="height: 85vh" alt="Airplane Image" />
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-white fs-1 text-center pb-5">Computer Programmer</h5>
+                </div>
+            </div>
+
+            <div class="carousel-item" style="height: 85vh" data-bs-interval="3000">
+                <g-image src="https://i.imgur.com/p2ncO1v.jpg" class="d-block w-100 item" style="height: 85vh" alt="Airplane Image" />
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-white fs-1 text-center pb-5">Nature Lover</h5>
+                </div>
+            </div>
         </div>
+        <a class="carousel-control-prev" href="#hero" role="button" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#hero" role="button" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </a>
     </div>
 </template>
 
 <style scoped>
-#item {
-    background: linear-gradient(rgba(20,20,20, .5),rgba(20,20,20, 1));
+
+div #hero {
     height: 85vh;
+    width: 100vw;
+    background: black;
+    overflow: hidden;
 }
-#hero {
-    opacity: .5;
+
+img {
+   object-fit: cover;
+   opacity: 0.4;
 }
+
 </style>
