a98f3204eb2451784ce2a374f9e208d2d248c221
rmcfarlin <rmcfarlin@robertmcfarlin.com>
2020-12-23 14:33:20 -0500

    Changed about section

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,10 +8,10 @@
                 <h5 class="row fs-2 text-decoration-underline text-center pb-3 justify-content-center">About Me</h5>
                 <p class="row justify-content-center px-5">
                     My name is Robert McFarlin and I currently live in Greenville, SC. I am a Certified Management Accountant and love everything related to business. 
-                    I enjoy learning anything that I can and am a true believer in lifelong learning. 
+                    I enjoy learning anything that I can and am a true believer in lifelong education. 
                 </p>
-                <p class="row justify-content-center px-5">When I am not engrossed in a report or automating a process, you can find me spending time with my wife and children, 
-                    hanging around the local airport, or camping at many of the nearby state parks.</p>    
+                <p class="row justify-content-center px-5">When I am not engrossed in a report or automating a process, you can find me spending time with my wife and children 
+                    or camping at a local state park.</p>    
             </div>
         </div>
     </div>
