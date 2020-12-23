fe283df5aa741554b872b8f05c3ee36f6e430718
rmcfarlin <rmcfarlin@robertmcfarlin.com>
2020-12-23 14:30:14 -0500

    Changed hero order

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -15,16 +15,16 @@
             </div>
 
             <div class="carousel-item" style="height: 80vh" data-bs-interval="3000">
-                <g-image src="https://i.imgur.com/aUdNIKM.jpg" class="d-block w-100 item" style="height: 80vh" alt="Airplane Image" />
+                <g-image src="https://i.imgur.com/ET6N4ec.jpg" class="d-block w-100 item" style="height: 80vh" alt="Airplane Image" />
                 <div class="carousel-caption">
-                    <h5 class="text-white fs-1 text-center pb-5">Airplane Enthusiast</h5>
+                    <h5 class="text-white fs-1 text-center pb-5">Computer Programmer</h5>
                 </div>
             </div>
 
             <div class="carousel-item" style="height: 80vh" data-bs-interval="3000">
-                <g-image src="https://i.imgur.com/ET6N4ec.jpg" class="d-block w-100 item" style="height: 80vh" alt="Airplane Image" />
+                <g-image src="https://i.imgur.com/aUdNIKM.jpg" class="d-block w-100 item" style="height: 80vh" alt="Airplane Image" />
                 <div class="carousel-caption">
-                    <h5 class="text-white fs-1 text-center pb-5">Computer Programmer</h5>
+                    <h5 class="text-white fs-1 text-center pb-5">Airplane Enthusiast</h5>
                 </div>
             </div>
 
