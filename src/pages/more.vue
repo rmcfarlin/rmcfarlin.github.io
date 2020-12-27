ecac74cf382ae0d38d7568154e271075285d1755
rmcfarlin <rmcfarlin@robertmcfarlin.com>
2020-12-27 06:13:37 -0500

    More work on about me section

--- src/pages/more.vue
+++ src/pages/more.vue
@@ -1,10 +1,10 @@
 <template>
   <Layout>
     <BNav />
-        <div class="cover">
-            <div class="more-body">
-                <div class="mt-5 pt-5 row mx-auto bg-light">
-                    <div class="col-sm-12 col-lg-4">
+        <div class="container py-3 my-3 text-dark">
+            <div class="bg-over">
+                <div class="mt-3 pt-5 row mx-auto">
+                    <!-- <div class="col-sm-12 col-lg-4">
                         <div class="mt-5 card mx-auto" style="width: 18rem;">
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
@@ -12,32 +12,31 @@
                                 <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
-                    </div>
-
-                    <div class="mt-5 col-sm-12 col-lg-4">
-                        <div class="card mx-auto" style="width: 18rem;">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div> -->
+                    <div class="card bg-light rounded-0">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <h2 class="card-header bg-light text-center">About Me</h2>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-
-                    <div class="mt-5 col-sm-12 col-lg-4">
-                        <div class="card mx-auto" style="width: 18rem;">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <div class="row py-4 px-4">
+                                <div class="card col-sm-12 col-lg-6 mx-auto">
+                                    <div class="card-body">
+                                        <h4 class="card-header bg-white text-center mb-3">Resources</h4>
+                                    </div>
+                                </div>
+                                <div class="card col-sm-12 col-lg-4 mx-auto">
+                                    <div class="card-body">
+                                        <h4 class="card-header bg-white text-center mb-3">Blog</h4>
+                                        <p class="card-text bg-white fs-6">Blog</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
 
                 </div>
-
-
-
             </div>
         </div>
     <Footer />
@@ -60,21 +59,7 @@ export default {
 </script>
 
 <style scoped>
-.more-body {
-    background-image: url("https://starsinphotos.files.wordpress.com/2013/04/southern-cross-region-1s.jpg");
-    background-repeat: no-repeat;
-    height: 100vh;
-    width: 100vw;
-    background-color:#000;
-    background-position: center;
-    background-size: cover;
-    opacity: 0.1;
-}
-
-.cover {
-    height: 100vh;
-    width: 100vw;
-    background: black;
-    overflow: hidden;
+.bg-over {
+    height: 100vh !important;
 }
 </style>
